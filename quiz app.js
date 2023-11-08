ef81741f5f03807308d891f23cdf7850e4ba309a
suprithvenki <124327981+suprithvenki@users.noreply.github.com>
2023-11-08 14:10:50 +0530

    Add files via upload

---
+++ quiz app.js
@@ -0,0 +1,107 @@
+alert("put in your score in the comment box")
+ 
+   //Questions array
+            let questions = [
+            {
+                 number:1,
+                 question: "What does Jumba stand for",
+                 answer : "Highly energetic dance class",
+                 options : [
+                                  "A yoga class",
+                                  "Highly energetic dance class",
+                                  "singing class",
+                                  "Gymnastic class"
+                                ]
+            
+            },
+            {
+                 number:2,
+                 question: "Total sleep time needed by a human ?",
+                 answer : "6 hours a day",
+                 options : [
+                                  "7 hours a day",
+                                  "6 hours a day",
+                                  "9 Hours a day",
+                                  "11 hours a day"  
+                                  ]
+            
+            },
+           {
+                 number:3,
+                 question: "Why fitness  matters a most ?",
+                 answer : "all of the above ",
+                 options : [ 
+                                  "To live a long life",
+                                  "To be healthy",
+                                  "To live a happy and peaceful life ",
+                                  "all of the above "
+                                  ]
+            
+            },
+            {
+                 number:4,
+                 question: " What are the benefits of regular exercise ?",
+                 answer : " increased muscle strength",
+                 options : [
+                                  "free from body pain",
+                                  " increased muscle strength",
+                                  "increased body durability",
+                                  "none of the above"
+                                  ]
+            }, 
+            {
+                 number:5,
+                 question: " Why is stretching important before and after exercise ?",
+                 answer : "all of the above",
+                 options : [ 
+                                  "helps to warm up the muscles",
+                                  "reduce the risk of injury",
+                                  "increase flexibility",
+                                  "all of the above"
+                               ]
+            },
+            {
+                 number:6,
+                 question: "How many calories are there in one gram of protein ?",
+                 answer : "One gram of protein contains 4 calories",
+                 options : [
+                                  "One gram of protein contains 14 calories",
+                                  "One gram of protein contains 4 calories",
+                                  "One gram of protein contains 7 calories",
+                                  "One gram of protein contains 9 calories"
+                               ]
+            },
+            {
+                 number:7,
+                 question: "How much fat does a human body need ?",
+                 answer : "80 grams of fat or less a day",
+                 options : [
+                                  "80 grams of fat or less a day",
+                                  "95 grams of fat for a day",
+                                  "120 grams per day",
+                                  "none of the above"
+                               ]
+            },
+             {
+                 number:8,
+                 question: "practicing yoga helps in ?",
+                 answer : "all of the above",
+                 options : [
+                                 "Free up our mind",
+                                 "helps in focusing",
+                                 "contolled blood regulation",
+                                 "all of the above"
+                               ]
+            },
+            {
+                 number:9,
+                 question: "Fitness portal helps you to ?",
+                 answer : "all of the above",
+                 options : [
+                                 "Maintain the perfect physique",
+                                 "maintain the happy living",
+                                 "motivate you to reach your goals",
+                                 "all of the above"
+                               ]
+            }
+             ];    
